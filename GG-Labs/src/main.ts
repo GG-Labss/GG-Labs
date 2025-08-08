51690d34ff3dd8a6a6ef66d8eb1c74078c5b3128
Caio Diniz <caiodiniz200204@gmail.com>
2025-08-08 16:50:36 -0300

    Refactor footer layout and styles for improved organization and aesthetics; add Google icon to main application

--- GG-Labs/src/main.ts
+++ GG-Labs/src/main.ts
@@ -1,12 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-import { logoInstagram, logoLinkedin, logoGithub } from 'ionicons/icons'
+import { logoInstagram, logoLinkedin, logoGithub, logoGoogle } from 'ionicons/icons'
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { addIcons } from 'ionicons';
 
-addIcons({ 'logo-instagram': logoInstagram, 'logo-linkedin': logoLinkedin, 'logo-github': logoGithub });
+addIcons({ 'logo-instagram': logoInstagram, 'logo-linkedin': logoLinkedin, 'logo-github': logoGithub,});
 
 bootstrapApplication(AppComponent, {
   providers: [
