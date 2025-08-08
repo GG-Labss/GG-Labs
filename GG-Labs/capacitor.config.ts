f838c3083979bf7f64d8abbf3de84a73fd32bbda
Caio Diniz <caiodiniz200204@gmail.com>
2025-08-08 10:02:04 -0300

    Starting project

---
+++ GG-Labs/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'GG-Labs',
+  webDir: 'www'
+};
+
+export default config;
