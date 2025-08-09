16c4d376b069499e71747c38af6039c26e2d71fd
Vinícius Madureira Godinho de Oliveira <vinimagod@gmail.com>
2025-08-09 20:07:40 -0300

    Add: About, Portfolio, and Projects pages with routing and basic structure

--- GG-Labs/src/app/app.routes.ts
+++ GG-Labs/src/app/app.routes.ts
@@ -9,5 +9,17 @@ export const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },  {
+    path: 'about',
+    loadComponent: () => import('./Pages/about/about.page').then( m => m.AboutPage)
   },
+  {
+    path: 'projects',
+    loadComponent: () => import('./Pages/projects/projects.page').then( m => m.ProjectsPage)
+  },
+  {
+    path: 'portfolio',
+    loadComponent: () => import('./Pages/portfolio/portfolio.page').then( m => m.PortfolioPage)
+  },
+
 ];
